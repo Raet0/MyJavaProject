9a783d97f08c876c657f732485981e22ba980f33
raet0 <Yosoyrafaelprieto@gmail.com>
2024-05-29 18:02:57 -0500

    3 commit, implementacion de  arreglos

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        String books[] = {"horror","fantasy","Comedy","kids"};
+        int pages[] = {1,2,3,4,5,6,7,8,9,10};
+        
+    }
+
+}
