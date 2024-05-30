33009d2364801378cb10cd0a40865099a045c1bc
raet0 <Yosoyrafaelprieto@gmail.com>
2024-05-29 19:35:48 -0500

    6to commit llamada de las clases objeto

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import Controller.ObjectController;
+import Controller.ObjectView;
+
 public class Main {
     public static void main(String[] args) {
         
         String books[] = {"horror","fantasy","Comedy","kids"};
         int pages[] = {1,2,3,4,5,6,7,8,9,10};
-        
+        ObjectController objetito = new ObjectController();
+        ObjectView view = new ObjectView();
     }
 
 }
